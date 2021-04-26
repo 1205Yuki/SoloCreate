77cf13a984228566fb334d9293e931f8acefe398
1205Yuki <eiennnonibante@gmail.com>
2021-04-26 13:39:12 +0900

    Pa

--- Pazzle/Pazzle/main.cpp
+++ Pazzle/Pazzle/main.cpp
@@ -1,7 +1,16 @@
 #include <DxLib.h>
+#include "Application.h"
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR LpCmdLine, _In_ int nCmdShow)
 {
+	Application* App = Application::SInstance();
+
+	if (App->Init()==false)
+	{
+		return -1;
+	}
+	App->Run();
+	App->Fin();
 
 	return 0;
 }
